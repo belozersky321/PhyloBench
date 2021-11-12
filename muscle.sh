bfc78d57bd940fceb6ec09fb5e97d77ecd7440d2
saspirin <sas@belozersky.msu.ru>
2021-11-12 18:42:39 +0300

    Adding muscle.sh with comments

--- muscle.sh
+++ muscle.sh
@@ -1,10 +1,18 @@
+####################################################
+# This script is a part of PhyloBench
+# Run it after blast_ort.py
+# It requires the improved version of stable.py
+####################################################
+
+python=/usr/bin/python2.7 # !!! Change this to the actual path to Python 2.7 interpreter
 if [[ ! -d Alignments ]]
   then mkdir Alignments
 fi
 for x in `ls Sequences | sed 's/\.fasta$//'`
 do
-  muscle -in Sequences/$x.fasta -out temp.afa
-  python2.7 stable.py Sequences/$x.fasta temp.afa > Alignments/$x.afa
+  echo $x
+  muscle -in Sequences/$x.fasta -out temp.afa 2>> muscle.log
+  $python ../Scripts/stable.py Sequences/$x.fasta temp.afa > Alignments/$x.afa
   rm temp.afa
 done
 
